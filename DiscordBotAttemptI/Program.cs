c66c011ccaf188a289e2ccbba0aed12b043cf37b
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2020-11-23 17:31:19 -0500

    Second attempt

--- DiscordBotAttemptI/Program.cs
+++ DiscordBotAttemptI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
 using System.Reflection;
@@ -10,113 +11,77 @@ namespace DiscordBotAttemptI
 {
     public class Program
     {
-       public static void Main(string[] args)
-        
-            //Used to establish an async context
-           => new Program().MainAsync().GetAwaiter().GetResult();
-
+        // setup our fields we assign later
+        private readonly IConfiguration _config;
         private DiscordSocketClient _client;
 
-        public async Task MainAsync()                                    
+        static void Main(string[] args)
         {
-            _client = new DiscordSocketClient();
-            _client.Log += Log;
-
-            //  You can assign your bot token to a string, and pass that in to connect.
-            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-            var token = File.ReadAllText("token.txt");
-            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-            // var token = File.ReadAllText("token.txt");
-            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
-
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
-
-            //Block this task until the program is closed
-            await Task.Delay(-1);
+            new Program().MainAsync().GetAwaiter().GetResult();
         }
 
-        private Task Log(LogMessage msg)
+        public Program()
         {
-            Console.WriteLine(msg.ToString());
+            // create the configuration
+            var _builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile(path: "config.json");
+
+            // build the configuration and assign to _config          
+            _config = _builder.Build();
+        }
+
+        public async Task MainAsync()
+        {
+            // call ConfigureServices to create the ServiceCollection/Provider for passing around the services
+            using (var services = ConfigureServices())
+            {
+                // get the client and assign to client 
+                // you get the services via GetRequiredService<T>
+                var client = services.GetRequiredService<DiscordSocketClient>();
+                _client = client;
+
+                // setup logging and the ready event
+                client.Log += LogAsync;
+                client.Ready += ReadyAsync;
+                services.GetRequiredService<CommandService>().Log += LogAsync;
+
+                // this is where we get the Token value from the configuration file, and start the bot
+                await client.LoginAsync(TokenType.Bot, _config["Token"]);
+                await client.StartAsync();
+
+                // we get the CommandHandler class here and call the InitializeAsync method to start things up for the CommandHandler service
+                await services.GetRequiredService<CommandHandler>().InitializeAsync();
+
+                await Task.Delay(-1);
+            }
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
             return Task.CompletedTask;
         }
 
-        public class CommandHandler
+        private Task ReadyAsync()
         {
-            private readonly DiscordSocketClient _client;
-            private readonly CommandService _commands;
-
-            // Retrieve client and CommandService instance via ctor
-            public CommandHandler(DiscordSocketClient client, CommandService commands)
-            {
-                _commands = commands;
-                _client = client;
-            }
-
-            public async Task InstallCommandsAsync()
-            {
-                // Hook the MessageReceived event into our command handler
-                _client.MessageReceived += HandleCommandAsync;
-
-                // Here we discover all of the command modules in the entry 
-                // assembly and load them. Starting from Discord.NET 2.0, a
-                // service provider is required to be passed into the
-                // module registration method to inject the 
-                // required dependencies.
-                //
-                // If you do not use Dependency Injection, pass null.
-                // See Dependency Injection guide for more information.
-                await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
-            }
-
-            private async Task HandleCommandAsync(SocketMessage messageParam)
-            {
-                // Don't process the command if it was a system message
-                var message = messageParam as SocketUserMessage;
-                if (message == null) return;
-
-                // Create a number to track where the prefix ends and the command begins
-
-                int argPos = 0;
-
-                // Determine if the message is a command based on the prefix and make sure no bots trigger commands
-                if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
-                    message.Author.IsBot) return;
-
-                // Create a WebSocket-based command context based on the message
-                var context = new SocketCommandContext(_client, message);
-
-                // Execute the command with the command context we just
-                // created, along with the service provider for precondition checks.
-                await _commands.ExecuteAsync(
-                    context: context,
-                    argPos: argPos,
-                    services: null);
-            }
+            Console.WriteLine($"Connected as -> [{_client.CurrentUser}] :)");
+            return Task.CompletedTask;
         }
 
-        public class InfoModule : ModuleBase<SocketCommandContext>
+        // this method handles the ServiceCollection creation/configuration, and builds out the service provider we can call on later
+        private ServiceProvider ConfigureServices()
         {
-            [Command("say")]
-            [Summary("Echoes a message.")]
-            public Task SayAsync([Remainder][Summary("the text to echo")] string echo) => ReplyAsync(echo);
+            // this returns a ServiceProvider that is used later to call for those services
+            // we can add types we have access to here, hence adding the new using statement:
+            // using csharpi.Services;
+            // the config we build is also added, which comes in handy for setting the command prefix!
+            return new ServiceCollection()
+                .AddSingleton(_config)
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandler>()
+                .BuildServiceProvider();
         }
-
-        [Group("sample")]
-        public class SampleModule : ModuleBase<SocketCommandContext>
-        {
-            [Command("square")]
-            [Summary("Squares a number.")]
-            public async Task SquareAsync([Summary("the nuber to square.")] int num)
-            {
-                await Context.Channel.SendMessageAsync($"{num}^2 = {Math.Pow(num, 2)}");
-            }
-        }
-
-
-
-
     }
 }
